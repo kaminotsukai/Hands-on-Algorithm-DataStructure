29132ddd2a4ac47ec6216f62e4995f639f42c72a
kaminotsukai <japanbritish.1107@gmail.com>
2021-05-22 12:19:41 +0900

    p33 線形探索法（最小値を保持する）

--- chapter-3/linear_search.php
+++ chapter-3/linear_search.php
@@ -43,3 +43,17 @@ function linear_search_found_index($Ai, $V)
 }
 
 
+
+/**
+ * 線形探索法（最小値を求める）
+ */
+function linear_search_min($Ai)
+{
+    $min = PHP_INT_MAX;
+
+    for ($i = 0; $i < count($Ai) - 1; $i++) {
+        $min = min($Ai[$i], $Ai[$i + 1]);
+    }
+
+    return $min;
+}
