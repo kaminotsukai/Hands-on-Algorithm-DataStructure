d2aa6e23bb494175104014069790321b20138105
kaminotsukai <japanbritish.1107@gmail.com>
2021-05-22 12:01:21 +0900

    p30 線形探索法

---
+++ chapter-3/linear_search.php
@@ -0,0 +1,26 @@
+<?php
+
+$N = (int)trim(fgets(STDIN));
+$Ai = array_map('intval', explode(' ', trim(fgets(STDIN))));
+$V = (int)trim(fgets(STDIN));
+
+/**
+ * 線形探索法（基本）
+ * = 検索のアルゴリズムの一つ。 リストや配列に入ったデータに対する検索を行うにあたって、 先頭から順に比較を行い、それが見つかれば終了する。（wiki）
+ *
+ * Point
+ * - 判定問題はオンオフを切り替えるフラグを持たせる
+ * - しらみつぶしに全て調べ上げる
+ */
+function linear_search($Ai, $V)
+{
+    $ans = false;
+    foreach ($Ai as $a) {
+        if ($a === $V) $ans = true;
+    }
+    return $ans ? 'Yes' : 'No';
+}
+
+
+
+
